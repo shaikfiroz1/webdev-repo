ec9804a5ea9e2cfb210233a3073805356f7034cf
firoz shaik <firozdravid1@gmail.com>
2026-05-03 14:28:54 +0530

    feat : error callbacks

--- callback.js
+++ callback.js
@@ -1,7 +1,30 @@
-function placeorder (item , callback){
-    setTimeout( () => {
-     callback(`item is deliverd : ${item}`)
+// function placeorder (item , callback){
+//     setTimeout( () => {
+//      callback(`item is deliverd : ${item}`)
     
-    }, 1000);
+//     }, 1000);
+// }
+// placeorder("biryani", (msg)=> console.log(msg));
+
+function fetchData(userid , callback){
+    setTimeout ( () => {
+        if (userid ===0 ){
+            callback(new Error ("invalid userid"), null);
+            
+        }else {
+            callback(null , {
+                id: userid,
+                name : "firoz"
+            })
+        }
+    }, 500)
 }
-placeorder("biryani", (msg)=> console.log(msg));
+
+fetchData(1 , (err, data) =>{
+    if (err){
+        console.error("error :", err.message);
+        
+    }else {
+        console.error("Data : " , data);
+    }
+})
