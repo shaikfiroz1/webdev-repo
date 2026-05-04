b74ddb20a477e4bd66299110bf9d79e7d476459f
firoz shaik <firozdravid1@gmail.com>
2026-05-04 14:42:34 +0530

    feat : promises

---
+++ promise.js
@@ -0,0 +1,20 @@
+function fetchUser(userId){
+    return new Promise( (resolve , reject) => {
+
+      setTimeout(() => {
+        if (userId>0) {
+            resolve ( {
+                id: userId, 
+                name : "firoz",
+                city : "Berlin"
+            })
+        }else {
+            reject(new Error("invalid id"));
+       }
+        }, 2000);
+
+    });
+}
+let p=fetchUser(0); 
+p.then (result => console.log(result))
+.catch( error => console.log (error)); 
