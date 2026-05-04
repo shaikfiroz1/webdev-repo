d382c511cbf7dffa7dc6e3e0a5c55eec5f433128
firoz shaik <firozdravid1@gmail.com>
2026-05-04 15:19:48 +0530

    feat : simple resolved promise

--- promise.js
+++ promise.js
@@ -1,20 +1,31 @@
-function fetchUser(userId){
-    return new Promise( (resolve , reject) => {
+// function fetchUser(userId){
+//     return new Promise( (resolve , reject) => {
 
-      setTimeout(() => {
-        if (userId>0) {
-            resolve ( {
-                id: userId, 
-                name : "firoz",
-                city : "Berlin"
-            })
-        }else {
-            reject(new Error("invalid id"));
-       }
-        }, 2000);
+//       setTimeout(() => {
+//         if (userId>0) {
+//             resolve ( {
+//                 id: userId, 
+//                 name : "firoz",
+//                 city : "Berlin"
+//             })
+//         }else {
+//             reject(new Error("invalid id"));
+//        }
+//         }, 2000);
 
-    });
-}
-let p=fetchUser(0); 
-p.then (result => console.log(result))
-.catch( error => console.log (error)); 
+//     });
+// }
+// let p=fetchUser(0); 
+// p.then (result => console.log(result))
+// .catch( error => console.log (error))
+// .finally(() => {
+//     console.log("finally is called here" 
+//   })
+
+//creating a simple promise 
+
+const myPromise = new Promise( (resolve,reject) =>{
+    resolve("hello wolrd")
+}  );
+
+myPromise.then ( result =>  {console.log(result)});    
