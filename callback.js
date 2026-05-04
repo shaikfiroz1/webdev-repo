563545cf63ae443684b0c338adefaf5fe7491d86
firoz shaik <firozdravid1@gmail.com>
2026-05-04 14:24:28 +0530

    feat : callbacks

--- callback.js
+++ callback.js
@@ -6,25 +6,37 @@
 // }
 // placeorder("biryani", (msg)=> console.log(msg));
 
-function fetchData(userid , callback){
-    setTimeout ( () => {
-        if (userid ===0 ){
-            callback(new Error ("invalid userid"), null);
+// function fetchData(userid , callback){
+//     setTimeout ( () => {
+//         if (userid ===0 ){
+//             callback(new Error ("invalid userid"), null);
             
-        }else {
-            callback(null , {
-                id: userid,
-                name : "firoz"
-            })
-        }
-    }, 500)
-}
+//         }else {
+//             callback(null , {
+//                 id: userid,
+//                 name : "firoz"
+//             })
+//         }
+//     }, 500)
+// }
 
-fetchData(1 , (err, data) =>{
-    if (err){
-        console.error("error :", err.message);
+// fetchData(1 , (err, data) =>{
+//     if (err){
+//         console.error("error :", err.message);
         
-    }else {
-        console.error("Data : " , data);
-    }
-})
+//     }else {
+//         console.error("Data : " , data);
+//     }
+// })
+
+function step1(cb) { setTimeout(() => { console.log("Step 1 done"); cb() }, 500) }
+function step2(cb) { setTimeout(() => { console.log("Step 2 done"); cb() }, 500) }
+function step3(cb) { setTimeout(() => { console.log("Step 3 done"); cb() }, 500) }
+
+step1(() => {
+  step2(() => {
+    step3(() => {
+      console.log("All 3 steps done! ")
+    })
+  })
+}) 
