e84d89bcf6b57d3f34fc21031331500318ad2ba2
firoz shaik <firozdravid1@gmail.com>
2026-06-07 11:24:41 +0530

    feat : recursion for exponents

---
+++ recursion2.py
@@ -0,0 +1,14 @@
+def power (base , exp) :
+
+    if exp == 0 :
+        return 1
+    elif exp < 0 :
+        return 1/ power (base , -exp)
+    else :
+        return base * power (base , exp-1)
+    
+
+print (power(2,4))
+print (power(2,-3))
+print (power(2,-1))
+print (power(2,0))
