561a69f01ce95e0471d2043d5c96970f214661bd
firoz shaik <firozdravid1@gmail.com>
2026-06-08 22:04:45 +0530

    feat : tuple-unpacking

---
+++ tuples1.py
@@ -0,0 +1,4 @@
+t=("firoz", 25, "unicon", "chenani", "Germany")
+name , age , company , place , foreign =t
+
+print ("Name : " , name  , "age : ", age , "company :", company , "place : " , place , "foreign : " , foreign )
