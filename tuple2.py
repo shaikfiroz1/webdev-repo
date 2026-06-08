010a08893c47eb84a8d6101f8f627eb2657a6b0d
firoz shaik <firozdravid1@gmail.com>
2026-06-08 22:12:00 +0530

    feat : tuple * unpacking

---
+++ tuple2.py
@@ -0,0 +1,9 @@
+# unpacking the first , middle and last elements 
+
+
+t= (29, "firoz", "parimalla", "Mini", 26)
+my_age, *mine, mine_age =t
+
+print ("my age :" , my_age)
+print ("mine : " , mine)
+print ("mine_age : ", mine_age)
