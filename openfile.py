b75e993ff244d81ec03741b50c3d24abf9c86a97
firoz shaik <firozdravid1@gmail.com>
2026-06-27 15:12:04 +0530

    feat : file modes operation on files (R,W,A)

--- openfile.py
+++ openfile.py
@@ -1,3 +1,22 @@
 with open("test", "r") as f:
-    content = f.read()
-print (content)
+ print(f.readline())
+ print(f.readline())
+  
+
+#  line1 = f.readline()
+#  line2 = f.readline()
+
+# print (line1)
+# print (line2)
+
+
+# print (type(content ))
+# for i in range  (len(content)) :
+#    if i == 1 :
+#         print(content[i]) 
+
+# else :
+#     print ("there will be  noting here ")
+
+     
+   
---
+++ write.py
@@ -0,0 +1,7 @@
+with open("test", "w") as f:
+    f.write("hello world\n")
+    
+    f.write("hello world\n")
+
+with open("test", "a") as c :
+    c.write ("this is appended")
