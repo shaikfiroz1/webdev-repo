01e993491ea497d8e5f175d5129ce2acaa899d9f
firoz shaik <firozdravid1@gmail.com>
2026-06-28 13:21:01 +0530

    feat : eek() and tell() cursor movement in Python

--- seekandtell.py
+++ seekandtell.py
@@ -1,5 +1,13 @@
-with open ("test", "r") as f :
+# with open ("test", "r") as f :
 
-   print (f.seek(5,0))
+#    print (f.seek(5,0))
 
-   print (f.tell() )
+#    print (f.tell() )
+
+
+
+with open("test", "r") as f :
+    content = f.read ()
+    print (f.tell())
+    f.seek(0)
+    print (f.tell())
