dade4159870e540041a2a5d258664a0ea3b42dfd
firoz shaik <firozdravid1@gmail.com>
2026-06-29 22:28:59 +0530

    feat : reading in to csv file

--- commasv.py
+++ commasv.py
@@ -1,9 +1,18 @@
 import csv
 
-data = [{"name": "Alice", "score": 92}, {"name": "Bob", "score": 85}]
+# data = [{"name": "Alice", "score": 92}, {"name": "Bob", "score": 85}]
 
-with open ("context.csv", "w", newline="", encoding="utf-8 ") as f :
+# with open ("context.csv", "w", newline="", encoding="utf-8 ") as f :
 
-    writer=csv.DictWriter(f, fieldnames=["name", "score"])
-    writer.writeheader()
-    writer.writerows(data)
+#     writer=csv.DictWriter(f, fieldnames=["name", "score"])
+#     writer.writeheader()
+#     writer.writerows(data)
+
+
+
+
+with open ("context.csv", "r",  encoding="utf-8 ") as f :
+
+    writer=csv.DictReader(f)
+    for line in writer : 
+        print(line["name"], line ["score"])
