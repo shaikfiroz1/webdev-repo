00b86ab300748084fba254be9542851540ad1131
firoz shaik <firozdravid1@gmail.com>
2026-06-29 22:21:33 +0530

    feat : writing into csv file

---
+++ commasv.py
@@ -0,0 +1,9 @@
+import csv
+
+data = [{"name": "Alice", "score": 92}, {"name": "Bob", "score": 85}]
+
+with open ("context.csv", "w", newline="", encoding="utf-8 ") as f :
+
+    writer=csv.DictWriter(f, fieldnames=["name", "score"])
+    writer.writeheader()
+    writer.writerows(data)
