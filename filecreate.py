07586fd6e29bd5293a20026b22dab226c244342b
firoz shaik <firozdravid1@gmail.com>
2026-06-29 21:26:00 +0530

    feat : reading number of lines

--- filecreate.py
+++ filecreate.py
@@ -1,7 +1,14 @@
-with open ("create.txt", "a") as f :
-    f.write ("\nhello.firoz")
+# with open ("create.txt", "a") as f :
+#     f.write ("\nhello.firoz")
 
 
-with open ("create.txt", "r") as f1 :
-    content = f1.read()
-    print (content)
+# with open ("create.txt", "r") as f1 :
+#     content = f1.read()
+#     print (content)
+
+
+total =0 
+with open("create.txt", "r", encoding="utf-8") as f:
+    for line in f :
+        total  +=1
+print(total)
