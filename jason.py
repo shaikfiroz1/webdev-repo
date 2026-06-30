28fa316413111122de1d863b259b2777c1a5a938
firoz shaik <firozdravid1@gmail.com>
2026-06-30 14:42:40 +0530

    feat : working on the csv file

---
+++ jason.py
@@ -0,0 +1,26 @@
+import json
+from pathlib import Path 
+defaults = {"theme": "dark", "language": "en", "font_size": 14}  
+
+   
+
+config_path= Path("config.json")
+
+try :
+    with open (config_path, "r", encoding="utf-8") as f :
+
+        config = json.load(f)
+    print ("The file is loaded at the json")
+
+
+except FileNotFoundError:
+
+
+    config = defaults
+    print ("Config file not found - used defaults")
+
+except  json.JSONDecodeError as e :
+    config = defaults
+    print (f"the file is not the json {e}- used only defaults ")
+
+print (config)
