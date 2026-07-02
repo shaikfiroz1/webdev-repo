2ddfb1257c04fd7c5bfe5df72ce566830725ade3
firoz shaik <firozdravid1@gmail.com>
2026-07-02 20:50:52 +0530

    feat : static classes

--- TradingAccount.py
+++ TradingAccount.py
@@ -1,18 +1,28 @@
-class TradingAccount:
+# class TradingAccount:
 
-    def __init__(self, trader_name, capital):
+#     def __init__(self, trader_name, capital):
 
-        self.trader_name = trader_name
-        self.capital = capital
+#         self.trader_name = trader_name
+#         self.capital = capital
 
-    def add(self , profit ) :
-            self.capital +=profit
-
-
+#     def add(self , profit ) :
+#             self.capital +=profit
 
 
-firozAccount = TradingAccount("Firoz", 100000)
 
-firozAccount.add(300)
 
-print (firozAccount.capital)
+# firozAccount = TradingAccount("Firoz", 100000)
+
+# firozAccount.add(300)
+
+# print (firozAccount.capital)
+
+
+
+
+class TradingAccount: 
+    @staticmethod
+    def calculate_brokerage(amount) :
+        return (amount/100) *2 
+    
+print(TradingAccount.calculate_brokerage(200))
